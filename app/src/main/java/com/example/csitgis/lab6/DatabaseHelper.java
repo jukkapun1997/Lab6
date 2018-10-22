67321ee35ed7275f3654fa9e8f1f228939cc5ce5
jukkapun1997 <b0827808460>
2018-10-22 16:21:22 +0700

    Lab6

--- app/src/main/java/com/example/csitgis/lab6/DatabaseHelper.java
+++ app/src/main/java/com/example/csitgis/lab6/DatabaseHelper.java
@@ -6,7 +6,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
 
     private static final int DATABASE_VERSION = 1;
 
@@ -17,35 +20,86 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(Student.CREATE_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db,int i,int il){
+    public void onUpgrade(SQLiteDatabase db, int i, int il) {
         db.execSQL("DROP TABLE IF EXISTS " + Student.TABLE_NAME);
 
         onCreate(db);
     }
-    public void insertStudent(String id,String student){
+
+    public void insertStudent(String id, String student){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
-        values.put(Student.COLUMN_ID,id);
-        values.put(Student.COLUMN_NAME,student);
+        values.put(Student.COLUMN_ID, id);
+        values.put(Student.COLUMN_NAME, student);
 
-        db.insert(Student.TABLE_NAME,null,values);
+        db.insert(Student.TABLE_NAME, null, values);
         db.close();
     }
-    public Student getStudent(String id){
+
+    public Student getStudent(String id) {
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = db.query(Student.TABLE_NAME,
-                new String[]{Student.COLUMN_ID,Student.COLUMN_NAME},
+                new String[]{Student.COLUMN_ID, Student.COLUMN_NAME},
                 Student.COLUMN_ID + "=?",
-                new String[]{String.valueOf(id)},null,null,null,null);
+                new String[]{String.valueOf(id)}, null, null,null, null);
 
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+
+        Student student = new Student(
+                cursor.getString(cursor.getColumnIndex(Student.COLUMN_ID)),
+                cursor.getString(cursor.getColumnIndex(Student.COLUMN_NAME)));
+
+        cursor.close();
+        return student;
     }
 
+    public List<Student> getAllStudents() {
+        List<Student> students = new ArrayList<>();
+
+        String selectQuery = "SELECT * FROM " + Student.TABLE_NAME;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                Student student = new Student();
+                student.setId(cursor.getString(cursor.getColumnIndex(Student.COLUMN_ID)));
+                student.setName(cursor.getString(cursor.getColumnIndex(Student.COLUMN_NAME)));
+                students.add(student);
+            } while (cursor.moveToNext());
+        }
+
+        db.close();
+        return students;
+    }
+
+    public int updateStudent(Student student) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(Student.COLUMN_ID, student.getId());
+        values.put(Student.COLUMN_NAME, student.getName());
+
+        return db.update(Student.TABLE_NAME, values, Student.COLUMN_ID + " = ?",
+                new String[]{String.valueOf(student.getId())});
+    }
+
+    public  void deleteStudent(Student student){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Student.TABLE_NAME, Student.COLUMN_ID + " = ?",
+                new String[]{String.valueOf(student.getId())});
+        db.close();
+    }
 
 }
